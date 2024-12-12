a57ab27521ed9b3b98e869ebc4ca45bb2cdb731b
varadevishal07@gmail.com <varadevishal07@gmail.com>
2024-12-12 09:41:41 +0530

    finally done

--- client/src/Actions/User.js
+++ client/src/Actions/User.js
@@ -39,7 +39,7 @@ export const getUser = () => async (dispatch) => {
             type: GET_USER_REQUEST,
         });
 
-        const { data } = await axios.get("/api/v1/user");
+        const { data } = await axios.get("https://myportfolio-abgj.onrender.com/api/v1/user");
         
         dispatch({
             type: GET_USER_SUCCESS,
@@ -59,7 +59,7 @@ export const login = (email, password) => async (dispatch) => {
             type: LOGIN_USER_REQUEST,
         });
 
-        const { data } = await axios.post("/api/v1/login", {
+        const { data } = await axios.post("https://myportfolio-abgj.onrender.com/api/v1/login", {
             email,
             password
         }, {
@@ -87,7 +87,7 @@ export const logout = () => async (dispatch) => {
             type: LOGOUT_USER_REQUEST,
         });
 
-        const { data } = await axios.get("/api/v1/logout");
+        const { data } = await axios.get("https://myportfolio-abgj.onrender.com/api/v1/logout");
 
         dispatch({
             type: LOGOUT_USER_SUCCESS,
@@ -107,7 +107,7 @@ export const loadUser = () => async (dispatch) => {
             type: LOAD_USER_REQUEST,
         });
 
-        const { data } = await axios.get("/api/v1/me");
+        const { data } = await axios.get("https://myportfolio-abgj.onrender.com/api/v1/me");
 
         dispatch({
             type: LOAD_USER_SUCCESS,
@@ -127,7 +127,7 @@ export const updateUser = (name,email,password,skills,about) => async (dispatch)
             type: UPDATE_USER_REQUEST,
         });
 
-        const { data } = await axios.put("/api/v1/admin/update", {
+        const { data } = await axios.put("https://myportfolio-abgj.onrender.com/api/v1/admin/update", {
             name,
             email,
             password,
@@ -157,7 +157,7 @@ export const addTimeline = (title,description,date) => async (dispatch) => {
             type: ADD_TIMELINE_REQUEST,
         });
 
-        const { data } = await axios.post("/api/v1/admin/timeline/add", {
+        const { data } = await axios.post("https://myportfolio-abgj.onrender.com/api/v1/admin/timeline/add", {
             title,
             description,
             date
@@ -185,7 +185,7 @@ export const deleteTimeline = (id) => async (dispatch) => {
             type: DELETE_TIMELINE_REQUEST,
         });
 
-        const { data } = await axios.delete(`/api/v1/admin/timeline/${id}`);
+        const { data } = await axios.delete(`https://myportfolio-abgj.onrender.com/api/v1/admin/timeline/${id}`);
 
         dispatch({
             type: DELETE_TIMELINE_SUCCESS,
@@ -205,7 +205,7 @@ export const addProject = (title,url,image,description,techStack) => async (disp
             type: ADD_PROJECT_REQUEST,
         });
 
-        const { data } = await axios.post("/api/v1/admin/project/add", {
+        const { data } = await axios.post("https://myportfolio-abgj.onrender.com/api/v1/admin/project/add", {
             title,url,image,description,techStack
         }, {
             headers: {
@@ -231,7 +231,7 @@ export const deleteProject = (id) => async (dispatch) => {
             type: DELETE_PROJECT_REQUEST,
         });
 
-        const { data } = await axios.delete(`/api/v1/admin/project/${id}`);
+        const { data } = await axios.delete(`https://myportfolio-abgj.onrender.com/api/v1/admin/project/${id}`);
 
         dispatch({
             type: DELETE_PROJECT_SUCCESS,
@@ -251,7 +251,7 @@ export const contactUs = (name,email,message) => async (dispatch) => {
             type: CONTACT_US_REQUEST,
         });
 
-        const { data } = await axios.post("/api/v1/contact", {
+        const { data } = await axios.post("https://myportfolio-abgj.onrender.com/api/v1/contact", {
             name,email,message
         }, {
             headers: {
